cfa94e13bedaf493d54495f7d6be2fb5b67257b1
Katryna Yaworski <Katrynayaworski@gmail.com>
2023-07-26 21:18:55 -0500

    end points added to grab css and mainJS

--- server/server.js
+++ server/server.js
@@ -8,5 +8,13 @@ app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'../public/index.html'))
 })
 
+
+app.get('/js',(req,res) => {
+    res.sendFile(path.join(__dirname,'../public/main.js'))
+})
+
+app.get('/css',(req,res) => {
+    res.sendFile(path.join(__dirname,'../public/styles.css'))
+})
 app.listen(5000, () => console.log(`server running on 5000`))
 
